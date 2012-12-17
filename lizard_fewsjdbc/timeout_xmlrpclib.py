52d1ff502e41a8f0652f8e2ea6f6b8dcfd9ea6ae
ejnens <erikjan.vos@nelen-schuurmans.nl>
2012-12-17 11:55:04 +0100

    pep8

--- lizard_fewsjdbc/timeout_xmlrpclib.py
+++ lizard_fewsjdbc/timeout_xmlrpclib.py
@@ -26,15 +26,20 @@ class TimeoutTransport(xmlrpclib.Transport):
         # create a HTTP connection object from a host descriptor
         chost, self._extra_headers, x509 = self.get_host_info(host)
         #store the host argument along with the connection object
-        self._connection = host, httplib.HTTPConnection(chost, timeout=self.timeout)
+        self._connection = host, httplib.HTTPConnection(
+            chost,
+            timeout=self.timeout
+        )
         return self._connection[1]
 
 
 class TimeoutServerProxy(xmlrpclib.ServerProxy):
     def __init__(self, uri, timeout=socket._GLOBAL_DEFAULT_TIMEOUT,
                  *args, **kwargs):
-        kwargs['transport'] = TimeoutTransport(timeout=timeout,
-                                    use_datetime=kwargs.get('use_datetime', 0))
+        kwargs['transport'] = TimeoutTransport(
+            timeout=timeout,
+            use_datetime=kwargs.get('use_datetime', 0)
+        )
         xmlrpclib.ServerProxy.__init__(self, uri, *args, **kwargs)
 
 ServerProxy = TimeoutServerProxy
