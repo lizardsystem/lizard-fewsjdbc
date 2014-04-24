e2e81c0b3950d60d79ac74a6121dbf0ef4896ec0
Reinout van Rees <reinout@vanrees.org>
2014-04-24 15:46:38 +0200

    Zapped unneeded import

--- lizard_fewsjdbc/models.py
+++ lizard_fewsjdbc/models.py
@@ -20,7 +20,6 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import post_save
 from django.db.models.signals import post_delete
-from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 from lizard_map.operations import named_list
